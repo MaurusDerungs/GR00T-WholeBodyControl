efa55ee414404185fe03ec55dbe9938b93a46a54
emilien ghazal <emilienghazal@gmail.com>
2026-05-10 03:20:13 +0200

    Reapply "Simplify station motion browser"
    This reverts commit 5749ab31c3fe6ff3ccc162f8aac12b8a48de41e8.

--- tools/sonic_station/motion_registry.py
+++ tools/sonic_station/motion_registry.py
@@ -79,7 +79,6 @@ def _motion_info(source: str, motion_dir: Path) -> MotionInfo:
 
 def discover_motions() -> list[MotionInfo]:
     groups = (
-        ("curated", REFERENCE_ROOT / "dance_loop"),
         ("generated", REFERENCE_ROOT / "kimodo"),
         ("predefined", REFERENCE_ROOT / "example"),
     )
